253c2b817834d50fcc323d8d63610bb641a9cd89
BuildTools <unconfigured@null.spigotmc.org>
2026-01-21 16:45:17 +0100

    update test database

--- src/test/java/pl/milosnicyit/codewarehousebackend/CodeWarehouseBackendApplicationTests.java
+++ src/test/java/pl/milosnicyit/codewarehousebackend/CodeWarehouseBackendApplicationTests.java
@@ -2,8 +2,10 @@ package pl.milosnicyit.codewarehousebackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CodeWarehouseBackendApplicationTests {
 
 	@Test
