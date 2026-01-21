35f6c1f66b4d011d1392f50cb2b3399ed80a5418
BuildTools <unconfigured@null.spigotmc.org>
2026-01-21 16:11:02 +0100

    first commit

---
+++ src/main/java/pl/milosnicyit/codewarehousebackend/CodeWarehouseBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.milosnicyit.codewarehousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWarehouseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeWarehouseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/milosnicyit/codewarehousebackend/CodeWarehouseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.milosnicyit.codewarehousebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWarehouseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
